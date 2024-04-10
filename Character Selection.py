7b1c07056a0e0c75edacd20daa9bce39da29afde
Jai <165696381+L0ve4Jai@users.noreply.github.com>
2024-04-10 13:26:42 -0500

    Add files via upload

---
+++ Character Selection.py
@@ -0,0 +1,9 @@
+Intro = print("Welcome to Slay The Dragon™!")
+
+Menu = str(input("In a realm shrouded in the mists of legend, the mighty kingdom of [KINGDOM NAME] once reigned supreme, its borders guarded by the valor of its knights and the wisdom of its sovereign, [KING NAME]. Yet, under the cloak of night, a shadow darker than any before descended upon the land. From the depths of ancient lore emerged [DRAGON NAME], a creature of fire and fury, whose very name struck fear into the hearts of all who heard it. Whispers of the dragon's rampage spread like wildfire, challenging the very foundation of the kingdom's strength. But amidst the chaos, three heroes emerged, their resolve unyielding in the face of insurmountable odds. Driven by a steadfast courage, they vowed to ascend the treacherous peaks of the forbidden mountain where the dragon dwelled, First among them was [PALADIN NAME], a stalwart defender whose unwavering faith in righteousness served as a beacon of hope amidst the encroaching shadows. Alongside the paladin strode [WIZARD NAME], a master of arcane arts whose intellect and mastery of magic were matched only by their unyielding determination to protect the realm from all manner of arcane threats. Completing the trio was [CLERIC NAME], a devoted servant of the gods whose healing hands and sacred prayers provided solace to those in need, bolstering the spirits of their comrades in the face of adversity. Please choose a mighty character to wield; Paladin, Wizard, Cleric: "))
+if Menu == "Cleric":
+        print("You have chosen [CLERIC NAME], Strength: Healing, Weakness: Damage")
+elif Menu == "Paladin":
+    print("You have chosen [PALADIN NAME], Strength: Damage, Weakness: Healing")
+elif Menu == "Wizard":
+     print("You have chosen [WIZARD NAME], Strength: Healing, Damage Weakness: Extended cooldown ")
