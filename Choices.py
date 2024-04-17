65cca76d6f72eaa342d7f5cfe4a1fa8aa425e788
Ninja3Nick <166568893+Ninja3Nick@users.noreply.github.com>
2024-04-17 11:30:35 -0500

    Update Choices.py

--- Choices.py
+++ Choices.py
@@ -28,7 +28,8 @@ print("Be brazen, you lead on a trechourous path.")
 Inventory = {"Slot 1":"Healing" , "Slot 2":"Damage Buff", "Slot 3":"Tradable Items", "Slot 4": "Collectibles"}
 print(Inventory)
 
-Weapons = {" Knight's Arsenal":"Sword/Spear", "Wizard's Arsenal":"Staff and Spells","Paladin's Arsenal": " A bow and arrow, along with the healing and damage remedies."}
+
+Weapons = {" Knight's Arsenal":"Sword/Spear", "Wizard's Arsenal":"Staff and Spells(Healing and Damage)","Paladin's Arsenal": " A bow and arrow, along with the healing and damage remedies."}
 
 print(Weapons)
 '''
